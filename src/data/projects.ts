30bb8053d4ac349f542da54c5cf06f2a7490cd1a
ichsandwihimawan <ichsandwihimawan@gmail.com>
2021-05-20 12:46:40 +0700

    oke

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,9 +3,9 @@ import { ProjectCardDataProps } from "components/molecules/ProjectCard/types";
 const projects: ProjectCardDataProps[] = [
   {
     title: "Projek Marketplace",
-    description: "Website Marketplace laravel+Vuejs.",
+    description: "Website Adversting alatberat Using Technology Laravel + Boostrap",
     type: "github",
-    url: "https://github.com/ichsandwihimawan/Project-MarketPlace",
+    url: "https://github.com/projekkalatberat/alatberat",
   },
   {
     title: "Portfolio",
@@ -15,10 +15,17 @@ const projects: ProjectCardDataProps[] = [
   },
   {
     title: "Overflow",
-    description: "TailwindCSS Admin Dashboard Template.",
-    type: "dribbble",
+    description: "https://github.com/ichsandwihimawan/Richfarmfix",
+    type: "github",
     url:
       "https://dribbble.com/shots/11877395-Overflow-TailwindCSS-Admin-Dashboard-Template",
+  },
+
+   {
+    title: "Portfolio",
+    description: "React JS + NODE JS (GOLDEN WISATA)",
+    type: "github",
+    url: "https://github.com/ichsandwihimawan/goldengolden",
   },
 ];
 
